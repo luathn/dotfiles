ff98ea460d13f4b4b3a6717e912726bc281a280a
luathn <luat.hoangn@gmail.com>
2019-10-24 13:49:30 +0700

    Init dotfiles: zsh, tmux, vim config

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+```
+chmod +x setup.sh
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,4 @@
+ln -s -f ~/dotfiles/zshrc ~/.zshrc
+ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
+rm -rf ~/.vim
+ln -s -f ~/dotfiles/vim/ ~/.vim
