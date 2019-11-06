8a8a5990b1518733a268be1a6c7fc2dd505ebe88
luathn <luat.hoangn@gmail.com>
2019-11-06 23:43:10 +0700

    Use urxvt is default terminal

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ ln -s -f ~/dotfiles/zshrc ~/.zshrc
 ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
 rm -rf ~/.vim
 ln -s -f ~/dotfiles/vim/ ~/.vim
+ln -s -f ~/dotfiles/Xresources ~/.Xresources
 
 vim +PlugInstall +qall
