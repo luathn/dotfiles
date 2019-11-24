d2e4cb7298ae3cd056e2c0a929d13de64a115eac
luathn <luat.hoangn@gmail.com>
2019-11-25 02:45:27 +0700

    Add bspwm, sxhkd, polybar. Change some vimrc, dunst, rofi

---
+++ config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/mybar.log
+polybar mybar >>/tmp/mybar.log 2>&1 &
+
+echo "Bars launched..."
