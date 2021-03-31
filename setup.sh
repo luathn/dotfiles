239b74f9a9dba8a26613d950bc5caa2c4c03fc79
luathn <luat1203@gmail.com>
2021-03-31 11:17:26 +0700

    Add gitignore global

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -s -f ~/dotfiles/zshrc ~/.zshrc
 ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/ctags ~/.ctags
 ln -s -f ~/dotfiles/gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/gitignore_global ~/.gitignore_global
 
 rm -r -f ~/.config/nvim
 ln -s -f ~/dotfiles/vim ~/.config/nvim
