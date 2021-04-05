6f9b53b4459cec266c3fe01cc294c95cf17f3efe
luathn <luat.hoangn@gmail.com>
2021-04-05 21:15:02 +0700

    Using zsh-defer to improve zsh performance

---
+++ zsh/zsh-export-path.zsh
@@ -0,0 +1,24 @@
+# Add rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+if command -v rbenv 1>/dev/null 2>&1; then
+  eval "$(rbenv init -)"
+fi
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+
+# Add pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
+# Yarn
+if command -v yarn 1>/dev/null 2>&1; then
+  export PATH="$PATH:$HOME/.yarn/bin"
+fi
+
+# Cargo
+export PATH="$PATH:$HOME/.cargo/bin"
+
+# Go
+export PATH=$PATH:/usr/local/go/bin
---
+++ zsh/zsh-fzf.zsh
@@ -0,0 +1,5 @@
+export FZF_DEFAULT_COMMAND="fd --type file --follow --no-ignore --hidden --exclude .git --exclude node_modules"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS="--inline-info"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
