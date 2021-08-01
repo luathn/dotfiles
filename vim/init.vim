a6349a7098847c355845a5d7451d313dcba43251
luathn <luat1203@gmail.com>
2021-08-01 22:28:00 +0700

    Nvim: Dap config for debug nestjs

--- vim/init.vim
+++ vim/init.vim
@@ -249,19 +249,3 @@ source ~/dotfiles/vim/config/function.vim
 " source ~/dotfiles/vim/config/fzf-tags.vim
 " source ~/dotfiles/vim/config/ale.vim
 " source ~/dotfiles/vim/config/indentline.vim
-
-nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
-nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
-nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
-nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
-nnoremap <silent> <leader>dr :lua require'dap'.continue()<CR>
-nnoremap <silent> <leader>ds :lua require'dap'.disconnect()<CR>
-nnoremap <silent> <leader>db :lua require'dap'.toggle_breakpoint()<CR>
-nnoremap <silent> <leader>dB :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
-nnoremap <silent> <leader>dp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
-" nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
-nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
-
-nnoremap <leader>dk :lua require("dapui").eval(<c-r>=expand("<cword>")<cr>)<CR>
-vnoremap <leader>dk :lua require("dapui").eval()<CR>
-
--- vim/lua/dap_debug_config.lua
+++ vim/lua/dap_debug_config.lua
@@ -1,98 +1,112 @@
 local dap = require('dap')
-dap.adapters.ruby = {
-  type = 'executable';
-  command = 'bundle';
-  args = {'exec', 'readapt', 'stdio'};
-}
 
-dap.configurations.ruby = {
-  {
-    type = 'ruby';
-    request = 'launch';
-    name = 'Rails';
-    program = 'bundle';
-    programArgs = {'exec', 'rails', 's'};
-    useBundler = true;
-  },
-}
+vim.api.nvim_set_keymap('n', '<leader>dd', "<cmd>lua require('dap').continue()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dq', "<cmd>lua require('dap').disconnect()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>db', "<cmd>lua require('dap').toggle_breakpoint()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dB', "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dB', "<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dr', "<cmd>lua require('dap').repl.toggle()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dl', "<cmd>lua require('dap').run_last()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<F5>', "<cmd>lua require('dap').continue()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<F10>', "<cmd>lua require('dap').step_over()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<F11>', "<cmd>lua require('dap').step_into()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<S-F11>', "<cmd>lua require('dap').step_out()<CR>", {silent=true, noremap=true})
 
-dap.defaults.fallback.external_terminal = {
-  command = '/usr/bin/alacritty';
-  args = {'-e'};
-}
-
-
-
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = {os.getenv('HOME') .. '/dotfiles/.dapinstalljsnode_dbg/vscode-node-debug2/out/src/nodeDebug.js'},
-}
-
-dap.configurations.javascript = {
-  {
-    type = 'node2',
-    request = 'launch',
-    -- program = '${file}',
-    program = '${workspaceFolder}/${file}',
-    -- outFiles = {'${fileDirname}/*.ts'},
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-}
-
-dap.configurations.typescript = {
-  {
-    type = 'node2',
-    request = 'launch',
-    localRoot = "${workspaceFolder}",
-    remoteRoot = "${workspaceFolder}",
-    program = 'npm run start:dev',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-}
-
+vim.api.nvim_set_keymap('n', '<leader>dt', "<cmd>lua require('dapui').toggle()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>de', "<cmd>lua require('dapui').eval()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('v', '<leader>de', "<cmd>lua require('dapui').eval()<CR>", {silent=true, noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>df', "<cmd>lua require('dapui').float_element(scopes)<CR>", {silent=true, noremap=true})
 
 -- Dap UI
-require("dapui").setup({
+require('dapui').setup({
   icons = {
-    expanded = "▾",
-    collapsed = "▸"
+    expanded = "",
+    collapsed = ""
   },
   mappings = {
     -- Use a table to apply multiple mappings
-    expand = {"<CR>", "<2-LeftMouse>"},
+    expand = {"<CR>", "<2-LeftMouse>", "<Tab>"},
     open = "o",
     remove = "d",
     edit = "e",
+    repl = "r",
+    close_floating = {"<Esc>", "q"},
   },
   sidebar = {
     open_on_start = true,
     elements = {
       -- You can change the order of elements in the sidebar
-      "scopes",
       "breakpoints",
-      "stacks",
-      "watches"
+      "scopes",
+      -- "stacks",
+      "watches",
     },
     width = 40,
-    position = "left" -- Can be "left" or "right"
+    position = "left"
   },
   tray = {
     open_on_start = true,
-    elements = {
-      "repl"
-    },
-    height = 10,
-    position = "top" -- Can be "bottom" or "top"
+    elements = { "repl" },
+    height = 9,
+    position = "bottom" -- Can be "bottom" or "top"
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
     max_width = nil   -- Floats will be treated as percentage of your screen.
   }
 })
+
+-- Nodejs
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/.dapinstall/jsnode_dbg/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+
+dap.configurations.typescript = {
+  {
+    name = 'Attach start:debug',
+    type = 'node2',
+    request = 'attach',
+    protocol = 'inspector',
+    hostName = "127.0.0.1",
+    port = 9229,
+    processId = '${command:PickProcess}',
+  },
+  -- {
+  --   name = 'Launch start:debug',
+  --   type = 'node2',
+  --   request = 'launch',
+  --   -- program = '${workspaceFolder}/node_modules/@nestjs/cli/bin/nest.js',
+  --   program = '${workspaceFolder}/src/main.ts',
+  --   args = {'start', '--debug', '--watch'},
+  --   cwd = vim.fn.getcwd(),
+  --   sourceMaps = true,
+  --   outFiles = {'${workspaceFolder}/dist/**/*.js'},
+  --   protocol = 'inspector',
+  --   console = 'integratedTerminal',
+  -- },
+}
+
+-- -- Ruby
+-- dap.adapters.ruby = {
+--   type = 'executable';
+--   command = 'bundle';
+--   args = {'exec', 'readapt', 'stdio'};
+-- }
+
+-- dap.configurations.ruby = {
+--   {
+--     type = 'ruby';
+--     request = 'launch';
+--     name = 'Rails';
+--     program = 'bundle';
+--     programArgs = {'exec', 'rails', 's'};
+--     useBundler = true;
+--   },
+-- }
+
+-- dap.defaults.fallback.external_terminal = {
+--   command = '/usr/bin/alacritty';
+--   args = {'-e'};
+-- }
