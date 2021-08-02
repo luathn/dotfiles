103765be44c8732662d0bc9bf5579a810c54125d
luathn <luat1203@gmail.com>
2021-08-02 11:58:13 +0700

    Nvim: Add efm-langserver

--- vim/lua/lsp_config.lua
+++ vim/lua/lsp_config.lua
@@ -77,3 +77,31 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = false,
   }
 )
+
+-- Eslint
+local util = require "lspconfig".util
+
+local eslint = {
+  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+  lintStdin = true,
+  lintFormats = {"%f:%l:%c: %m"},
+  lintIgnoreExitCode = true,
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatStdin = true
+}
+
+require "lspconfig".efm.setup {
+  init_options = {documentFormatting = true},
+  filetypes = {"javascript", "typescript"},
+  root_dir = function(fname)
+    return util.root_pattern("tsconfig.json")(fname) or
+    util.root_pattern(".eslintrc.js", ".git")(fname);
+  end,
+  settings = {
+    rootMarkers = {".eslintrc.js", ".git/"},
+    languages = {
+      javascript = {eslint},
+      typescript = {eslint}
+    }
+  }
+}
