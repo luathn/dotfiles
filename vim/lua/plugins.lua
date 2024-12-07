fe23c208e7af8c8f9a46168bfaa09d11c0457b08
luathn <luat1203@gmail.com>
2024-12-07 16:53:49 +0700

    update: remove none-ls.nvim plugin

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -208,18 +208,6 @@ return {
   },
   -- Test
   {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.prettier,
-        },
-      })
-    end,
-  },
-  {
     'akinsho/git-conflict.nvim',
     version = "*",
     event = "VeryLazy",
