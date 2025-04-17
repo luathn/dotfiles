ceb7f1f30d76284b909918f91b58ebce835373ca
luathn <luat1203@gmail.com>
2025-04-17 16:53:34 +0700

    feat(plugins): Add aider.nvim plugin for AI assistance

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -191,6 +191,21 @@ return {
       require("plugins.snacks")
     end
   },
+  {
+    "joshuavial/aider.nvim",
+    config = function()
+      require('aider').setup({
+        -- your configuration comes here
+        -- if you don't want to use the default settings
+        auto_manage_context = true, -- automatically manage buffer context
+        default_bindings = false,    -- use default <leader>A keybindings
+        debug = false,              -- enable debug logging
+      })
+
+      vim.api.nvim_set_keymap('n', '<leader>Aa', ':AiderOpen --no-auto-commits<CR>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles<CR>', {noremap = true, silent = true})
+    end
+  },
   -- {
   --   "hrsh7th/nvim-cmp",
   --   event = "VeryLazy",
